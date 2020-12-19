d5a738b55da2dc2ecd43ac718c13bae41532186c
keita0667 <chorus1717@gmail.com>
2020-12-19 20:34:27 +0900

    usersテーブル作成完了ver1

---
+++ db/migrate/20201219103709_add_birthdates_to_users.rb
@@ -0,0 +1,5 @@
+class AddBirthdatesToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :birth_date, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_113904) do
+ActiveRecord::Schema.define(version: 2020_12_19_103709) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_12_18_113904) do
     t.string "lastname_kanji"
     t.string "firstname_kana"
     t.string "lastname_kana"
+    t.date "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
