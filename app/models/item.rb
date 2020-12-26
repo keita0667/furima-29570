fff7211e536dc17f8cf0946c9cd4eb8323dd6ae6
keita0667 <chorus1717@gmail.com>
2020-12-26 20:57:33 +0900

    修正ver5

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,15 @@
 class Item < ApplicationRecord
 
+  belongs_to :user, optional: true
+  has_one_attached :image
+
   with_options presence: true do
     validates :image
     validates :name
     validates :info
     validates :price
   end
-  
+
     validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'}
 
@@ -19,10 +22,9 @@ class Item < ApplicationRecord
   end
   
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :prefecture
-  belongs_to :sales_status
-  belongs_to :scheduled_delivery
-  belongs_to :shipping_fee_status
-  has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :scheduled_delivery
+  belongs_to_active_hash :shipping_fee_status
 end
