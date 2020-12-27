0801835ac688e42c1f8563399a5608792cab881a
keita0667 <chorus1717@gmail.com>
2020-12-27 18:59:40 +0900

    編集機能ver4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :item_define, only: [:show, :edit]
+  before_action :item_define, only: [:show, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
